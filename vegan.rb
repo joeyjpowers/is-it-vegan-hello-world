00bcac6f5153ad6500c477e96b143c7c7df1073e
JonathanMaskew <jmaskew@purdue.edu>
2020-10-17 21:07:37 -0400

    Updated HTML

--- vegan.rb
+++ vegan.rb
@@ -1208,20 +1208,22 @@ __END__
 </head>
 
 <body>
-	<header>
-            <h1>Is It Vegan?</h1>
-            <p>
-                  Wondering if what you want to eat meets your vegan diet? 
-                  <br>
-                  Input a food item below and we'll let you know if it's vegan.
-            </p>
-      </header>
+header>
+<h1>Is It Vegan?</h1>
+    <p>
+      Wondering if what you want to eat meets your vegan diet? 
+      <br>
+      Input a food item or UPC number below and we'll let you know if it's vegan.
+      <br>
+      <span style="font-style: italic;">Note that UPC numbers provide more specific results, so we recommend using a UPC number if available.</span>
+    </p>
+</header>
 
-	<form action="/", method = "post">
-		<input type="text" placeholder="Food Item" required id="food", name="desc"/>
-            <br>
-		<button id="submit">Check if Vegan...</button>
-	</form>
+<form action="/", method = "post">
+    <input type="text" placeholder="Food item or UPC number" required id="food", name="desc"/>
+    <br>
+    <button id="submit">Check If Vegan...</button>
+</form>
         <div><p id="result"><%= @result1 %></p></div>
         <div><p id="result"><%= @result %></p></div>
         <div><p id="food"><%= @food %></p></div>
